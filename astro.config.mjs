bbc19b7db336acdce8c19265f7b4734c4f7fd6b8
arbasyaa <arbasyaa@gmail.com>
2026-02-27 20:12:19 +0700

    git-pages-update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://arbasyaa.github.io', // Tambahkan ini (Domain utama Anda)
+  base: '/lp-bintang-roster-jaya',    // Tambahkan ini (Nama folder/repo Anda)
   image: {
-    domains: ['images.unsplash.com'],
+    domains: ['images.unsplash.com'], // Ini biarkan saja, penting untuk gambar
   },
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss()],         // Ini biarkan saja, penting untuk desain
   },
 });
